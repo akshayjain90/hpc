ac0d253dfd888998907574a1d5db779f283b6379
uwhpsc <uwhpsc@ubuntu.(none)>
2015-04-24 12:11:03 -0700

    hw3 updated

--- submissions/hw3/gs-omp.c
+++ submissions/hw3/gs-omp.c
@@ -11,11 +11,14 @@ double compute_residual(double *lu, int lN, double invhsq){
 	int i;
 	double tmp;
 	lres=0.0;
-	#pragma omp for reduction(+:lres)
-	for (i = 1; i < lN; i++){
-		tmp = ((2.0*lu[i] - lu[i-1] - lu[i+1]) * invhsq - 1);
-		lres += tmp * tmp;
-	}	
+	#pragma omp parallel			
+	{
+		#pragma omp for reduction(+:lres)
+		for (i = 1; i < lN; i++){
+			tmp = ((2.0*lu[i] - lu[i-1] - lu[i+1]) * invhsq - 1);
+			lres += tmp * tmp;
+		}	
+	}
 	return sqrt(lres);
 }
 
@@ -25,6 +28,7 @@ int main(int argc, char** argv){
 		printf("Need a value for N and number of iterations\n");
 		exit(1);
 	}
+	
 
 	int N = atoi(argv[1]), i,it, iter = atoi(argv[2]), tid;
 	double *u1, *u2;
@@ -37,24 +41,31 @@ int main(int argc, char** argv){
 	
 	for(it=0; it<iter; it++){			
 
-		if(it %2 ==0){			
-			#pragma omp for schedule(dynamic,100)
-			for(i=1; i< N+1; i=i+2){				
-				u2[i] = aiiInv + u1[i-1]/2 + u1[i+1]/2;		
-			}
-			#pragma omp for schedule(dynamic,100)
-			for(i=2; i< N+1; i=i+2){				
-				u2[i] = aiiInv + u2[i-1]/2 + u2[i+1]/2;		
+		if(it %2 ==0){
+			#pragma omp parallel			
+			{			
+				#pragma omp for schedule(dynamic,100)
+				for(i=1; i< N+1; i=i+2){				
+					u2[i] = aiiInv + u1[i-1]/2 + u1[i+1]/2;		
+				}
+			
+				#pragma omp for schedule(dynamic,100)
+				for(i=2; i< N+1; i=i+2){				
+					u2[i] = aiiInv + u2[i-1]/2 + u2[i+1]/2;		
+				}
 			}
 		} else {
-			#pragma omp for schedule(dynamic,100)
-			for(i=1; i< N+1; i=i+2){				
-				u1[i] = aiiInv + u2[i-1]/2 + u2[i+1]/2;		
+			#pragma omp parallel			
+			{
+				#pragma omp for schedule(dynamic,100)
+				for(i=1; i< N+1; i=i+2){				
+					u1[i] = aiiInv + u2[i-1]/2 + u2[i+1]/2;		
+				}
+				#pragma omp for schedule(dynamic,100)
+				for(i=2; i< N+1; i=i+2){				
+					u1[i] = aiiInv + u1[i-1]/2 + u1[i+1]/2;		
+				}	
 			}
-			#pragma omp for schedule(dynamic,100)
-			for(i=2; i< N+1; i=i+2){				
-				u1[i] = aiiInv + u1[i-1]/2 + u1[i+1]/2;		
-			}	
 		}
 		
 		if(it %100 ==0){		
@@ -63,6 +74,10 @@ int main(int argc, char** argv){
 				printf("Iter %d:Residual: %g \n", it, res);
 		}
 	}
+
+        res= compute_residual(u2,N+1,ihsq);
+        printf("Iter %d:Residual: %g \n", iter, res);
+
 	free(u1);
 	free(u2);
 
